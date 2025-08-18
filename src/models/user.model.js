f52293dc77c91e0957011456f186188499d24a6f
Puneet Yadav <puneety259@gmail.com>
2025-08-18 23:44:57 +0530

    Refactor authentication routes: improve response status codes and enhance login error handling

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password:{
-        type: String,
-        required: true,
-    }
-
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 const userModel = mongoose.model("user", userSchema);
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -9,23 +9,54 @@ router.get("/user", (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   const existingUser = await userModel.find({ email });
 
-  if (!existingUser) {
+  if (existingUser) {
     return res.status(400).json({ msg: "user is already exists!" });
   }
-
   const user = await userModel.create({
     email,
     password,
   });
 
-  const token = jwt.sign({user: user._id, email}, process.env.JWT_SECRET);
+  const token = jwt.sign(
+    { user: user._id, email: user.email },
+    process.env.JWT_SECRET
+  );
 
-  res.status(201).json({ msg: "user registered!", user , token});
+  res.status(201).json({ msg: "user registered!", user, token });
+});
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    res.status(404).json({
+      message: " User not found ",
+    });
+  }
+
+  const isPasswordValid = password === user.password;
+
+  if (!isPasswordValid) {
+    res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET
+  );
+
+  res.status(200).json({
+    message: " User logged In Successfully.",
+    user,
+    token,
+  });
 });
 
 module.exports = router;
